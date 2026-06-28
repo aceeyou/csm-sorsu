97ed8fdae1da830a2810a67f3c77e3840a384413
Ace Logronio <acearwingl@gmail.com>
2026-06-28 19:06:30 +0800

    module exported server/src/indexjs

--- server/src/index.js
+++ server/src/index.js
@@ -87,6 +87,7 @@ app.post("/api/postcsmresponse", verification, async (req, res) => {
 // });
 
 connectDB();
-app.listen(1337, (req, res) => {
-  console.log("Listening on port 1337");
-});
+// app.listen(1337, (req, res) => {
+//   console.log("Listening on port 1337");
+// });
+module.exports = app;
