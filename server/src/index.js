a4d847792047e7ebd110c648367a6e182ef7f58c
Ace Logronio <acearwingl@gmail.com>
2026-07-01 23:58:53 +0800

    Re-config CORS Access Control Allow Origin and allowed methods and headers

--- server/src/index.js
+++ server/src/index.js
@@ -19,13 +19,21 @@ import typesRouter from "./routes/TypesOfOffices.routes.js";
 import officeRouter from "./routes/Office.routes.js";
 import serviceRouter from "./routes/Service.routes.js";
 
+const corsOptions = {
+  origin: "https://csm-sorsu.vercel.app",
+  optionsSuccessStatus: 200,
+  methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+};
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 dotenv.config();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://csm-sorsu.vercel.app");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS",
