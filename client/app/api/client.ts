8db7a9b88dd04a85ce547f57c1a12324a4876152
Ace Logronio <acearwingl@gmail.com>
2026-07-02 10:31:45 +0800

    Configured More CORS

--- client/app/api/client.ts
+++ client/app/api/client.ts
@@ -5,7 +5,7 @@ export const apiClient = axios.create({
   //   baseURL: "http://127.0.0.1:1337",
   baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   headers: {
-    "Content-Type": "application/json",
+    // "Content-Type": "application/json",
     // "Allow-Control-Allow-Origin": "http://127.0.0.1:3000", // Allow requests from any origin
     "Allow-Control-Allow-Origin": "https://csm-sorsu.vercel.app", // Allow requests only from the specified origin
   },
